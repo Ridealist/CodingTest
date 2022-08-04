3eb04b3215df57d55996db8fcb51cdc6eaca5e29
Ridealist <cjsn92@gmail.com>
2022-08-04 17:05:07 +0900

    TIL: Dynamic Programming Exercises_2.

---
+++ LeetCode_study/1D_Dynamic_Programming/min_cost_climbing_stairs.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/min-cost-climbing-stairs/
+"""
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        cost.append(0)
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1] + cost[i + 2])
+
+        return min(cost[0], cost[1])
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(2, len(cost)):
+            if cost[i - 1] > cost[i - 2]:
+                cost[i] += cost[i - 2]
+            elif cost[i - 1] <= cost[i - 2]:
+                cost[i] += cost[i - 1]
+        return cost[-1]
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        cost.append(0)
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return min(cost[-1], cost[-2])
