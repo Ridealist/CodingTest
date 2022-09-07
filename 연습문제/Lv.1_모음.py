adce606c35efcb3453a437db41b498710cb91de1
Ridealist <cjsn92@gmail.com>
2022-09-07 23:48:55 +0900

    TIL: CodingTest Exercises.

---
+++ 연습문제/Lv.1_모음.py
@@ -0,0 +1,17 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/12931
+"""
+
+def solution(n):
+    return sum([int(i) for i in str(n)])
+
+# map 아이디어
+def solution(n):
+    return sum(map(int, str(n)))
+
+# 재귀 아이디어
+def solution(n):
+    if n >= 10:
+        r = n % 10
+        return r + solution(n // 10)
+    return n
