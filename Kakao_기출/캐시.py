c764dd7e1dab289b17b558bfa53fcc9d1ee8bec4
Ridealist <cjsn92@gmail.com>
2022-09-25 00:18:43 +0900

    TIL: CodingTes 다른 풀이 공부 추가

--- Kakao_기출/캐시.py
+++ Kakao_기출/캐시.py
@@ -55,54 +55,36 @@ def solution(cacheSize, cities):
 
 import collections
 
-## 회택 풀이
-# class LRUCache:
-#     def __init__(self, max_size):
-#         self.max_size = max_size
-#         self.dic = collections.OrderedDict()
-#         self.runtime = 0
-    
-#     def get(self, key):
-#         if key not in self.dic:
-#             return -1
-#         val = self.dic[key]
-#         self.dic.move_to_end(key, last=True)
-#         self.runtime += 1
-#         return val
-
-#     def put(self, key, value):
-#         self.dic[key] = value
-#         self.runtime += 5
-#         self.dic.move_to_end(key)
-#         if len(self.dic) > self.max_size:
-#             self.dic.move_to_end(last=False)
+# 회택 풀이
+import collections
 
 class LRUCache:
     def __init__(self, max_size):
         self.max_size = max_size
-        self.dic = dict()
+        self.dic = collections.OrderedDict()
+        self.runtime = 0
     
-    def get(self, key:int):
-        if key in self.dic:
-            value = self.dic.pop(key)
-            self.dic[key] = value # pop 이후 다시 입력하면 가장 뒷 순서로 배치
-            return value
-        else:
+    def get(self, key):
+        if key not in self.dic:
             return -1
-    
-    def put(self, key:int, value:int):
+        val = self.dic[key]
+        self.dic.move_to_end(key, last=True)
+        self.runtime += 1
+        return val
+
+    def put(self, key, value):
         self.dic[key] = value
+        self.runtime += 5
+        self.dic.move_to_end(key)
         if len(self.dic) > self.max_size:
-            self.dic.pop(list(self.dic.keys())[0])
+            self.dic.popitem(last=False)
 
+            
+def solution(cacheSize, cities):
+    cache = LRUCache(cacheSize)
+    for city in cities:
+        city = city.lower()
+        if cache.get(city) == -1:
+            cache.put(city, city)
+    return cache.runtime
 
-lru = LRUCache(3)
-
-print(lru.get(1))
-print(lru.put(1, "서울"))
-print(lru.put(2, "대구"))
-lru.put(3,"부산")
-lru.put(4, "전주")
-
-lru.get(3)
-print(lru.dic)
