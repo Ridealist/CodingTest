5f84eafce4e3efcb221a5968dcd012b7695f8576
Ridealist <cjsn92@gmail.com>
2022-10-02 21:40:35 +0900

    TIL: 스터디원 풀이 공부

--- CodingTest_문제풀이/프로그래머스_3단계/섬연결하기.py
+++ CodingTest_문제풀이/프로그래머스_3단계/섬연결하기.py
@@ -40,3 +40,35 @@ n = 4
 costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
 
 print(solution(n, costs))
+
+
+#### 회택 풀이
+
+import heapq
+
+def solution(n, costs):
+    graph = [[] for _ in range(n)]
+    for cost in costs:
+        node = cost[0]
+        target = cost[1]
+        c = cost[2]
+        graph[node].append((target, c))
+        graph[target].append((node, c))
+    
+    q = []
+    visited = set()
+    visited.add(0)
+    for i in graph[0]:
+        heapq.heappush(q, (i[1], i[0]))
+    
+    min_cost = 0
+    while len(visited) < n:
+        weight, node = heapq.heappop(q)
+        if node in visited:
+            continue
+
+        min_cost += weight
+        visited.add(node)
+        for i in graph[node]:
+            heapq.heappush(q, (i[1], i[0]))
+    return min_cost
